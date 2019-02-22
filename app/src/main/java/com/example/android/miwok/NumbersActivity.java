abf684892ad7694a381cbdbff7d11149ae963e27
jsisanova <jana.sisanova@gmail.com>
2019-02-22 17:32:24 +0100

    Use a ListView and custom ArrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,17 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +20,103 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create a list of words
+
+        ArrayList<Word> words = new ArrayList<Word>();
+//        Word w = new Word("one", "lutti");
+//        words.add(w);
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo’e"));
+        words.add(new Word("ten", "na’aacha"));
+
+
+//        ArrayList<String> words = new ArrayList<>(Arrays.asList("one","two","three","four",
+//                "five","six","seven","eight","nine","ten"));
+
+//        ArrayList<String> words = new ArrayList<String>();
+//        words.add("one");
+//        words.add("two");
+//        words.add("three");
+//        words.add("four");
+//        words.add("five");
+//        words.add("six");
+//        words.add("seven");
+//        words.add("eight");
+//        words.add("nine");
+//        words.add("ten");
+
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+//        ArrayAdapter<Word> itemsAdapter =
+//                new ArrayAdapter<Word>(this, R.layout.list_item, words);
+
+//        ArrayAdapter<String> itemsAdapter =
+//                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+//        listView.setAdapter(itemsAdapter);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
+
+
+
+        // Find the root view so we can add child views to it
+//        LinearLayout rootView = findViewById(R.id.rootView);
+        // Keep looping
+//        for (int i = 0; i < words.size(); i++) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(i));
+//            rootView.addView(wordView);
+//        }
+
+//        int i = 0;
+//        while(i<words.size()) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(i));
+//            rootView.addView(wordView);
+//            i++;
+//        }
+
+//        for(String word: words) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(word);
+//            rootView.addView(wordView);
+//        }
+
+
+        // Verify the contents of the list by printing out each element to the logs
+//        for (String word: words) {
+//            Log.v("NumbersActivity", "Word at index " + words.indexOf(word) + ": " + word);
+//        }
+
+//        for(int i = 0; i < words.size(); i++)  {
+//            Log.v("NumbersActivity", "Word at index " + i +": " + words.get(i));
+//        }
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,70 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+// WordAdapter is a custom adapter that takes as it's input a list of Word objects
+// So when the list of item views is requested, it will find the view at the correct position and then
+// create or reuse a list item layout. SO the views will be updated based on the information in the Words object
+// and then the list item view is returned to ListView.
+public class WordAdapter extends ArrayAdapter<Word> {
+
+/** @param context  The current context. Used to inflate the layout file.
+    @param words    A List of AndroidFlavor objects to display in a list
+*/
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+        // we are calling ArrayAdaptor's (superclass') constructor here
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position The position in the list of data that should be displayed in the
+     *                 list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    // Get a list item view that we can use
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // EITHER by reusing - get the list item = {@link Word} object located at this position in the list
+        View listItemView = convertView;
+        // OR inflate (= create) new list item view from list_item.xml
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        //Get the {@link Word} object located at this position in the list
+        Word currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView miwokWord = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        TextView defaultWord = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Populate the data into the template view using the data object
+        miwokWord.setText(currentWord.getMiwokTranslation());
+        defaultWord.setText(currentWord.getDefaultTranslation());
+        // Return the whole list item layout (containing 2 TextViews) from the word object
+        // so that it can be shown in the ListView on screen
+        return listItemView;
+    }
+
+}
+
+
+
+
